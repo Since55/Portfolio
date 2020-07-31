4a938950642c09001ce33ab8cec53c93a94b9423
Ruslan Ludchak <heckfyua@gmail.com>
2020-07-31 23:43:14 +0300

    Updates

---
+++ precache-manifest.b4130e240463d22187a4b16bb02d29df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f593c2873246aecb871159d1f040c6e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9ac183ee9b40b5da7925",
+    "url": "/portfolio/static/css/main.8264f8cd.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac183ee9b40b5da7925",
+    "url": "/portfolio/static/js/main.5f94b309.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "39b2701ac77adcc58463173073f217c2",
+    "url": "/portfolio/static/media/person.39b2701a.jpg"
+  }
+]);
